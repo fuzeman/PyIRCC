ea508fdeb7d858d8f93d9aa8173a9da05b136de5
Dean Gardiner <gardiner91@gmail.com>
2013-01-02 22:43:11 +1300

    Added [setup.py]

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='PyIRCC',
+      version='0.1',
+      description='IRCC (Sony) network remote control library',
+      author='Dean Gardiner',
+      author_email='gardiner91@gmail.com',
+      url='http://github.com/fuzeman/PyIRCC',
+      packages=['pyircc'],
+)
